8dde9edddb8f8c88d9c87546fb275be214531f9f
unemotioned <134937260+unemotioned@users.noreply.github.com>
2026-07-17 18:26:31 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Student Grader
+
+Simple **C++** project to practice **OOP**.
+
+## Features
+
+- Read students information
+- Calculate total and average
+- Assign grade
+- Rank by grade
+- Print results and output as json file
+
+## TODOs
+
+- [x] Read file
+- [x] Calculate total and average
+- [ ] Grade
+- [ ] Sort
+- [ ] Output JSON
+- [ ] Refactor
+- [ ] Draw flow chart
+
+---
+
+## Compile and Run
+
+```sh
+mkdir -p build
+
+g++ ./src/main.cpp -o ./build/main
+
+./build/main
+```
+
+---
+
+## Makefile
+
+[Makefile Tutorial](https://makefiletutorial.com/)
+
+After making changes to source codes.
+
+```sh
+make
+
+./build/main
+```
---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+// Copyright (c) 2025 UnEmotioneD
+
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <sstream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::istringstream;
+using std::list;
+using std::string;
+
+class Student {
+  public:
+    int num;
+    string name;
+    int kor;
+    int eng;
+    int math;
+    int tot;
+    float avg;
+    char grade;
+    int rank;
+};
+
+int main() {
+    // Read file
+    ifstream studentFile;
+    studentFile.open("./student_scores.txt");
+
+    // Create student class
+    Student student;
+    list<Student> students;
+
+    // Assign contents of file to string variable
+    string studentInfo = "";
+
+    // read from studentFile
+    // for each line
+    // assign it to studentInfo
+    while (getline(studentFile, studentInfo)) {
+        istringstream iss(studentInfo);
+        iss >> student.num >> student.name >> student.kor >> student.eng >>
+            student.math;
+
+        student.tot = student.kor + student.eng + student.math;
+        student.avg = static_cast<float>(student.tot) / 3;
+
+        students.push_back(student);
+        studentInfo = "";
+    }
+    studentFile.close();
+
+    for (Student student : students) {
+        cout << student.name << " - average score: " << student.avg << endl;
+    }
+
+    return 0;
+}
